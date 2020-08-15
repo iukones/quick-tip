e994be5850e7d797801ab2f0543a84e1862b7fff
iukones <iukones1@gmail.com>
2020-08-14 20:39:03 -0500

    add styles inputs and range input

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function update() {
   let bill = document.getElementById('yourBill').value;
   let tipPercent = document.getElementById('tipInput').value;
   let split = document.getElementById('splitInput').value;
-  console.log({bill, tipPercent, split});
+  // console.log({bill, tipPercent, split});
 }
 
 let container = document.getElementById('container');
+
 container.addEventListener('input', update);
